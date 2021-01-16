67504c745e7e41cc822311a4b996be18a8b27620
tirth0 <tirtharajsengupta@gmail.com>
2021-01-16 14:27:59 +0530

    auth

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/auth/:providerName/callback" component={LoginRedirect}/>
+        <Route path="/connect/:providerName/redirect" component={LoginRedirect}/>
         <Route exact path="/" component={Login}/>
         <Route exact path="/Home" component={Application}/>
         <Route exact path="/Roads" component={Roads}/>
