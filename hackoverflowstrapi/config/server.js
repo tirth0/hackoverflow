3ea4032dca55e6bb9e771c0c8a393a69f06fde1b
tirth0 <tirtharajsengupta@gmail.com>
2021-01-16 11:49:14 +0530

    changes

--- hackoverflowstrapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b1243e02ad5aef281301b829994892f1'),
-    },
-  },
-});
--- hackoverflowstrapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '1cd466c7-f943-43f9-94e8-7c41871f1cc3'
-};
